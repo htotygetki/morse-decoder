8d75d59cd590d88f41920fec64f17574e435b2a7
Roman <92747747+htotygetki@users.noreply.github.com>
2022-08-04 15:46:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
+function decode(expr) {
+    // write your solution here
+
+	 let arr = expr.split('**********');
+  let arr2 = [];
+  let arrWithoutZir = [];
+  let arrStr = [];
+  let strItem = '';
+  let str = '';
+  let number = 0;
+
+  for (item of arr){
+		for (let i = 0; i < item.length; i+=10){
+			arr2.push(item.slice(i, i+10));
+		}
+		arr2.push(' ');
+	}
+
+	arr2.pop();
+
+  for (item of arr2){
+	while (item[0] == '0'){
+		item = item.slice(1);
+	}
+	arrWithoutZir.push(item);
+  }
+
+  for(item of arrWithoutZir){
+	strItem = '';
+	if (item == ' ') arrStr.push(' ');
+	else{
+		for (let i = 0; i < item.length/2; i++){
+			if (item[2*i] + item[2*i+1] == '11'){
+				strItem = strItem.concat('-');
+			}
+			else {
+				strItem = strItem.concat('.');
+			}
+		}
+		arrStr.push(strItem);
+	}
+  }
+
+  for (item of arrStr){
+	if (item == ' ') str+=' ';
+	else{
+		number = Object.keys(MORSE_TABLE).indexOf(item);
+		str += (Object.values(MORSE_TABLE))[number];
+	}	
+  }
+ 
+
+  return str;
+}
+
+module.exports = {
+    decode
+}
+
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
